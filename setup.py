887584c0da27ff64bf0316f84baf392dd9a3669e
Will Morris <willmorris103@gmail.com>
2024-08-04 03:22:47 -0500

    Prepare for release

--- setup.py
+++ setup.py
@@ -3,7 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='home_assistant_litedb',
     version='0.1.0',
-    license='MIT',
     packages=find_packages(),
     install_requires=[
         'requests',
